f205579e34b93d6c56dc56d1a829e79d7386e6da
Kareem Rady <kareem_rady@yahoo.com>
2016-06-03 23:23:20 +0000

    Added a helper add_to_products method and error checking for duplicate products

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,28 @@
 class Product
+  attr_reader :title
   @@products = []
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+
+
+
+  private
+
+  def add_to_products
+  if @@products.reduce(false) {|bool_value, product| bool_value || product.title == @title}
+    raise ProductDuplicateError: "#{@title} already exists"
+  else
+    @@products << self
+  end
+end
+
+
+
 end
